dbab8563b45c1f9ea20bfb54a2194d55b2ce4dab
geethama <geeth4862@gmail.com>
2023-07-18 23:35:46 +0530

    add put and delete mapping

--- src/main/java/com/example/photoclone/PhotoController.java
+++ src/main/java/com/example/photoclone/PhotoController.java
@@ -1,25 +1,51 @@
 package com.example.photoclone;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
-import java.util.List;
+import java.util.*;
 
 @RestController
 public class PhotoController{
 
-    private List<Photo> db = List.of(new Photo("1", "hello.jpg"));
+    //    private List<Photo> db = List.of(new Photo("1", "hello.jpg"));
 
+    private Map<String, Photo> db = new HashMap<>(){
+        {
+            put("1", new Photo("1", "Hello.jpg"));
+        }
+    };
 
     @GetMapping("/")
     public String Hello(){
-        System.out.println(db);
         return "Hello World";
     }
 
+    @GetMapping("/photos")
+    public Collection<Photo> get(){
+        System.out.println(db.values());
+        return db.values();
+    }
+
     @GetMapping("/photos/{id}")
-    public List<Photo> get(@PathVariable String id){
-        return db;
+    public Photo get(@PathVariable String id){
+        Photo photo = db.get(id);
+        if (photo == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        return photo;
+    }
+
+    @DeleteMapping("/photo/{id}")
+    public Photo delete(@PathVariable String id){
+        Photo photo = db.remove(id);
+        if (photo == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        return photo;
+    }
+
+    @PutMapping("/photos/add")
+    public Photo create(Photo photo){
+        photo.setId(UUID.randomUUID().toString());
+        db.put(photo.getId(), photo);
+        return  photo;
     }
 }
